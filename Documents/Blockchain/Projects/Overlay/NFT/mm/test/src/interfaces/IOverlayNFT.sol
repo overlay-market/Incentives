a2223440989d9534167bf26ea6b928c80ae0f67d
c-note <dongustavo20@gmail.com>
2023-05-01 16:17:50 +0100

    clean up

--- Documents/Blockchain/Projects/Overlay/NFT/mm/test/src/interfaces/IOverlayNFT.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.13;
-
-interface IOverlayNFT {
-    function setStakingContract(address _newStakingContract) external;
-
-    function mintTo(address _recipient) external;
-}
