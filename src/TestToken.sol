b8760ef8ab61745596de2b06b986764a942d1cdd
c-note <dongustavo20@gmail.com>
2023-05-03 04:29:59 +0100

    added token lock up test cases and token demo contract

---
+++ src/TestToken.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.13;
+
+import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+contract TestToken is ERC20 {
+    constructor() ERC20("TestToken", "TT") {
+        _mint(_msgSender(), 100000 * (10**uint256(decimals())));
+    }
+
+    function faucet(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+}
---
+++ test/TokenLockUpTest.t.sol
@@ -0,0 +1,167 @@
+import "forge-std/Test.sol";
+import "../src/TestToken.sol";
+import "../src/OverlayNFT.sol";
+import "../src/TokenLockUp.sol";
+import "openzeppelin-contracts/contracts/token/ERC721/IERC721Receiver.sol";
+
+contract TokenLockUpTest is Test {
+    // Declare the necessary variables
+    IERC20 token;
+    OverlayNFT nftContract;
+    TokenLockUp tokenLockUp;
+    uint256 lockDuration = 1000;
+
+    function setUp() public {
+        // Deploy the TokenLockUp contract
+        token =  new TestToken();
+        nftContract = new OverlayNFT('_name', '_symbol', '_baseURI');
+        tokenLockUp = new TokenLockUp(address(token), address(nftContract));
+
+        // Approve the token to be used for deposit
+        token.approve(address(tokenLockUp), 1000);
+    }
+
+    function testDeposit() public {
+        tokenLockUp.setDepositDeadline(10000);
+        nftContract.setStakingContract(address(tokenLockUp));
+
+        // Deposit 1000 tokens with a lockup period of 1000 seconds
+        tokenLockUp.deposit(1000, lockDuration);
+
+        // Assert that the user has received 2 NFTs
+        assertEq(nftContract.balanceOf(address(this)), 2);
+
+        // Assert that the user has 1 locked batch of tokens
+        assertEq(tokenLockUp.getUserLockedBatchTokenCount(), 1);
+    }
+
+    function testWithdrawTokens() public {
+        tokenLockUp.setDepositDeadline(10000);
+        nftContract.setStakingContract(address(tokenLockUp));
+
+        uint userBalanceBeforeDepositTx = token.balanceOf(address(this));
+
+        // Deposit 1000 tokens with a lockup period of 1000 seconds
+        tokenLockUp.deposit(1000, lockDuration);
+
+        uint userBalanceAfterDepositTx = token.balanceOf(address(this));
+
+        // Advance the block timestamp to the end of the lockup period
+        vm.warp(block.timestamp + lockDuration);
+
+        // Withdraw the tokens
+        tokenLockUp.withdrawTokens(0);
+
+        uint userBalanceAfterWithdrawTx = token.balanceOf(address(this));
+
+        // Assert that the user has 1 locked batches of tokens
+        assertEq(tokenLockUp.getUserLockedBatchTokenCount(), 1);
+
+        assertEq(userBalanceBeforeDepositTx - 1000, userBalanceAfterDepositTx);
+
+        // Assert that the user has received 1000 tokens
+        assertEq(userBalanceAfterDepositTx + 1000, userBalanceAfterWithdrawTx);
+    }
+
+    function testFailToWithdrawBeforeTokensAreUnlock() public {
+        uint256 amount = 1000;
+
+        tokenLockUp.setDepositDeadline(10000);
+        nftContract.setStakingContract(address(tokenLockUp));
+
+        tokenLockUp.deposit(amount, lockDuration);
+        tokenLockUp.withdrawTokens(0);
+    }
+
+    function testPause() public {
+        tokenLockUp.pause();
+
+        assert(tokenLockUp.paused());
+    }
+
+    function testUnpause() public {
+        tokenLockUp.pause();
+
+        assert(tokenLockUp.paused());
+
+        // Try to unpause before deposit deadline
+        tokenLockUp.setDepositDeadline(block.timestamp + 100);
+        tokenLockUp.unpause();
+
+        assert(!tokenLockUp.paused());
+    }
+
+    function testFailToUnpauseAfterDeadlinePassed() public {
+        tokenLockUp.pause();
+        tokenLockUp.setDepositDeadline(block.timestamp);
+        tokenLockUp.unpause();
+    }
+
+    function testFailWhenDepositingAfterDeadline() public {
+        tokenLockUp.setDepositDeadline(block.timestamp);
+
+        // Advance the block timestamp to the end of the lockup period
+        vm.warp(block.timestamp + lockDuration);
+        tokenLockUp.deposit(1000, 100);
+    }
+
+    // Test case for depositing with invalid lock duration
+    function testFailWhenDepositingWithInvalidLockDuration() public {
+        tokenLockUp.setDepositDeadline(block.timestamp);
+        tokenLockUp.deposit(1000, 0);
+    }
+
+    // Test case for depositing with 0 amount
+    function testFailToDepositWithZeroAmount() public {
+        tokenLockUp.setDepositDeadline(block.timestamp);
+        tokenLockUp.deposit(0, lockDuration);
+    }
+
+    // Test case for withdrawing already withdrawn tokens
+    function testFailWithWithdrawAlreadyWithdrawnTokens() public {
+        tokenLockUp.setDepositDeadline(block.timestamp);
+        nftContract.setStakingContract(address(tokenLockUp));
+
+        tokenLockUp.deposit(1000, lockDuration);
+
+        // Advance the block timestamp to the end of the lockup period
+        vm.warp(block.timestamp + lockDuration);
+
+        // Withdraw tokens
+        tokenLockUp.withdrawTokens(0);
+
+        // Attempt to withdraw already withdrawn tokens
+        tokenLockUp.withdrawTokens(0);
+    }
+
+    // Test case for withdrawing with invalid index
+    function testFailWhenWithdrawingWithInvalidIndex() public {
+        tokenLockUp.setDepositDeadline(block.timestamp);
+        nftContract.setStakingContract(address(tokenLockUp));
+
+        tokenLockUp.deposit(1000, lockDuration);
+
+        // Advance the block timestamp to the end of the lockup period
+        vm.warp(block.timestamp + lockDuration);
+
+        // Attempt to withdraw with invalid index
+        tokenLockUp.withdrawTokens(1);
+    }
+
+     function testFailWhenNonOwnerCallsRestrictedFunctions() public {
+        vm.prank(address(1));
+        tokenLockUp.setTokenAddress(address(2));
+
+        tokenLockUp.setDepositDeadline(block.timestamp);
+        tokenLockUp.setNftContractnAddress(address(2));
+    }
+
+    function onERC721Received(
+        address,
+        address,
+        uint256,
+        bytes calldata
+    ) external returns (bytes4){
+        return this.onERC721Received.selector;
+    }
+}
