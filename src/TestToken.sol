45dd9bf73117fa5f6aa1b4e2670ff02bbd15cf45
c-note <dongustavo20@gmail.com>
2023-05-03 12:10:41 +0100

    added prettier format

--- src/TestToken.sol
+++ src/TestToken.sol
@@ -6,7 +6,7 @@ import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 contract TestToken is ERC20 {
     constructor() ERC20("TestToken", "TT") {
-        _mint(_msgSender(), 100000 * (10**uint256(decimals())));
+        _mint(_msgSender(), 100000 * (10 ** uint256(decimals())));
     }
 
     function faucet(address to, uint256 amount) external {
